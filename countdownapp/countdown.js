c991be03290d7a127c6437ca739a7230723a0cce
SanjibKarunaChaitanyaGhosh <sanjibghosh791@gmail.com>
2026-01-15 07:41:32 +0530

    trying-inc,dic,reset

--- countdownapp/countdown.js
+++ countdownapp/countdown.js
@@ -15,3 +15,25 @@ function updateCountDown(){
     countdownEL.innerHTML=`${min}:${sec}`
     time--;
 }
+
+// button for timer reset
+const increment=document.getElementById("Increase");
+const reset=document.getElementById("reset");
+const decrement=document.getElementById("Decrease");
+
+let count=10;
+
+increment.onclick=function(){
+    count++;
+    countdownEL.innerHTML=count;
+}
+// inplace of innerHTML we can use textContent
+decrement.onclick=function(){
+    count--;
+    countdownEL.innerHTML=count;
+}
+
+reset.onclick=function(){
+    count=0;
+    countdownEL.innerHTML=count;
+}
