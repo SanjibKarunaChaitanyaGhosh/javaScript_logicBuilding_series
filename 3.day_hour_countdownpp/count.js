6196a150d9900158c34168a553ea743dfaa23e7c
SanjibKarunaChaitanyaGhosh <sanjibghosh791@gmail.com>
2026-01-16 07:48:15 +0530

    try_myBest

---
+++ 3.day_hour_countdownpp/count.js
@@ -0,0 +1,30 @@
+const endDate="7 July 2027 00:00 AM";
+
+document.getElementById("end-Date").innerText=endDate;
+
+const inputs=document.querySelectorAll("input");
+
+function clock(){
+    const end=new Date("endDate");
+    const now=new Date();
+    const diff=(end-now)/1000;
+    console.log(diff);
+
+    // DAY
+    inputs[0].value=Math.floor(diff/3600/24)
+    // HOUR
+    inputs[1].value=Math.floor(diff/3600)%24;
+    // MINUITE
+    inputs[2].value=Math.floor(diff/60)%60;
+    // SEC
+    inputs[3].value=Math.floor(diff)%60;
+
+}
+
+clock();
+
+setInterval(
+    ()=>{
+        clock()
+    },1000
+);
