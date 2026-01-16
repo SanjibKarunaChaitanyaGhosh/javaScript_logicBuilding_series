6cf81fbc28042751a0e72409b11dad80ef797083
SanjibKarunaChaitanyaGhosh <sanjibghosh791@gmail.com>
2026-01-16 19:35:35 +0530

    small-mistake,correction_done

--- 3.day_hour_countdownpp/count.js
+++ 3.day_hour_countdownpp/count.js
@@ -1,17 +1,17 @@
-const endDate="7 July 2027 00:00 AM";
+const endDate="7 July 2026 00:00 AM";
 
 document.getElementById("end-Date").innerText=endDate;
 
 const inputs=document.querySelectorAll("input");
 
 function clock(){
-    const end=new Date("endDate");
+    const end=new Date(endDate);
     const now=new Date();
     const diff=(end-now)/1000;
     console.log(diff);
 
     // DAY
-    inputs[0].value=Math.floor(diff/3600/24)
+    inputs[0].value=Math.floor(diff/3600/24);
     // HOUR
     inputs[1].value=Math.floor(diff/3600)%24;
     // MINUITE
